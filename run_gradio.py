be47ac8a52738a06f8971c0a08904f5627f2f938
SIC98 <shininchul98@gmail.com>
2023-08-07 05:01:53 +0900

    CleanUp code

--- run_gradio.py
+++ run_gradio.py
@@ -1,146 +1,9 @@
-from diffusers import StableDiffusionInpaintPipeline
 from PIL import Image, ImageOps
 import gradio as gr
 import torch
 import numpy as np
 
-pipe = StableDiffusionInpaintPipeline.from_pretrained(
-    # "runwayml/stable-diffusion-inpainting",
-    "stabilityai/stable-diffusion-2-inpainting",
-    torch_dtype=torch.float16,
-).to('cuda')
-
-def find_bottom(left_bottom, width):
-    x, y = left_bottom
-    search_list = []
-
-    for i in range((width // 256) - 1):
-        search_list.append((x + i * 256, y - 384))
-
-    if width % 256 != 0:
-        search_list.append((x + width - 512, y - 384))
-
-    return search_list
-
-
-def find_top(left_top, width):
-    x, y = left_top
-    search_list = []
-
-    for i in range((width // 256) - 1):
-        search_list.append((x + i * 256, y - 128))
-
-    if width % 256 != 0:
-        search_list.append((x + width - 512, y - 128))
-
-    return search_list
-
-def find_left(left_top, height):
-    x, y = left_top
-    search_list = []
-
-    for i in range((height // 256) - 1):
-        search_list.append((x - 128, y + i * 256))
-
-    if height % 256 != 0:
-        search_list.append((x - 128, y + height - 512))
-    
-    return search_list
-
-def find_right(right_top, height):
-    x, y = right_top
-    search_list = []
-
-    for i in range((height // 256) - 1):
-        search_list.append((x - 384, y + i * 256))
-
-    if height % 256 != 0:
-        search_list.append((x - 384, y + height - 512))
-    
-    return search_list
-
-def fill_bottom(left_bottom, right_bottom, image, mask_image):
-    search_list = find_bottom(left_bottom, right_bottom[0] - left_bottom[0])
-    image, mask_image = fill_search_list(search_list, image, mask_image)
-
-    left_bottom[1] += 128
-    right_bottom[1] += 128
-
-    return left_bottom, right_bottom, image, mask_image
-
-
-def fill_left(left_top, left_bottom, image, mask_image):
-    search_list = find_left(left_top, left_bottom[1] - left_top[1])
-    image, mask_image = fill_search_list(search_list, image, mask_image)
-
-    left_bottom[0] -= 128
-    left_top[0] -= 128
-
-    return left_top, left_bottom, image, mask_image
-
-def fill_right(right_bottom, right_top, image, mask_image):
-    search_list = find_right(right_top, right_bottom[1] - right_top[1])
-    image, mask_image = fill_search_list(search_list, image, mask_image)
-
-    right_top[0] += 128
-    right_bottom[0] += 128
-
-    return right_bottom, right_top, image, mask_image
-
-def fill_top(left_top, right_top, image, mask_image):
-    search_list = find_top(left_top, right_top[0] - left_top[0])
-    image, mask_image = fill_search_list(search_list, image, mask_image)
-
-    left_top[1] -= 128
-    right_top[1] -= 128
-
-    return left_top, right_top, image, mask_image
-
-
-def fill_search_list(search_list, image, mask_image):
-    for x, y in search_list:
-            # Create new mask
-            new_mask_image = Image.new('RGB', mask_image.size, (255, 255, 255))
-            mask_image_np = np.array(new_mask_image)
-
-            # Fill the mask
-            mask_image_np[y:y+512, x:x+512] = (0, 0, 0)
-
-            # Mask to PIL
-            new_mask_image = Image.fromarray(mask_image_np)
-
-            mask2_gray = new_mask_image.convert('L')
-            mask2_array = np.array(mask2_gray)
-
-            # Generate and merge image
-            generated_image = crop_image(image, mask_image, x, y, x + 512, y + 512)
-            image = merge_image(image, generated_image, x, y)
-
-            mask1_gray = mask_image.convert('L')
-            mask1_array = np.array(mask1_gray)
-
-            # Update mask_image
-            result_array = np.logical_and(mask1_array == 255, mask2_array == 255)
-            mask_image = Image.fromarray(np.uint8(result_array)*255)
-
-    return image, mask_image
-
-
-def crop_image(image, result_mask, x, y, m, n):
-
-    print(x, y, m, n)
-
-    cropped_image = image.crop((x, y, m, n))
-    cropped_mask = result_mask.crop((x, y, m, n))
-
-    image = pipe(prompt='', image=cropped_image, mask_image=cropped_mask, height=m-x, width=n-y).images[0]
-
-    return image
-
-
-def merge_image(image, generated_image, x, y):
-    image.paste(generated_image, (x, y))
-    return image
+from utils import fill_bottom, fill_top, fill_left, fill_right
 
 
 def posible_start_point(image):
@@ -148,11 +11,11 @@ def posible_start_point(image):
     return max(0, x - 512), max(0, y - 512)
 
 
-
 def image_mod(image, left_border, right_border, top_border, bottom_border):
     mask_image = Image.new('RGB', image.size, (0, 0, 0))
 
-    left_border, right_border, top_border, bottom_border = int(left_border), int(right_border), int(top_border), int(bottom_border)
+    left_border, right_border, top_border, bottom_border = int(
+        left_border), int(right_border), int(top_border), int(bottom_border)
 
     print(image.size)
     w, h = image.size
@@ -161,19 +24,25 @@ def image_mod(image, left_border, right_border, top_border, bottom_border):
     add_w = -result_w % 8
     add_h = -result_h % 8
 
-    border = (left_border, top_border, right_border + add_w, bottom_border + add_h)
+    border = (left_border, top_border, right_border +
+              add_w, bottom_border + add_h)
 
     image = ImageOps.expand(image, border, fill='white')
     mask_image = ImageOps.expand(mask_image, border, fill='white')
 
-    left_top, left_bottom, right_top, right_bottom = [left_border, top_border], [left_border, top_border + h], [left_border + w, top_border], [left_border + w, top_border + h]
+    left_top, left_bottom, right_top, right_bottom = [left_border, top_border], [
+        left_border, top_border + h], [left_border + w, top_border], [left_border + w, top_border + h]
 
-    for _ in range(0, 4):
-        left_bottom, right_bottom, image, mask_image = fill_bottom(left_bottom, right_bottom, image, mask_image)
-        left_top, left_bottom, image, mask_image = fill_left(left_top, left_bottom, image, mask_image)
-        left_top, right_top, image, mask_image = fill_top(left_top, right_top, image, mask_image)
-        right_bottom, right_top, image, mask_image = fill_right(right_bottom, right_top, image, mask_image)
-    
+    for _ in range(0, 2):
+        left_bottom, right_bottom, image, mask_image = fill_bottom(
+            left_bottom, right_bottom, image, mask_image)
+        left_top, left_bottom, image, mask_image = fill_left(
+            left_top, left_bottom, image, mask_image)
+        left_top, right_top, image, mask_image = fill_top(
+            left_top, right_top, image, mask_image)
+        right_bottom, right_top, image, mask_image = fill_right(
+            right_bottom, right_top, image, mask_image)
+
     width, height = image.size
     return image.crop((0, 0, width - add_w, height - add_h))
 
@@ -188,6 +57,6 @@ def image_mod(image, left_border, right_border, top_border, bottom_border):
 if __name__ == "__main__":
     # demo.launch()
     image = Image.open('./imges/image1.png')
-    image = image.resize((1024,1024))
-    image = image_mod(image, 512, 512, 512, 512)
-    image.save("./new_cube.png")
+    # image = image.resize((1024,1024))
+    image = image_mod(image, 256, 256, 256, 256)
+    image.save("./result.png")
