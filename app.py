c675ac549c961e3fabe384e3ed9e8793bb825622
SIC98 <shininchul98@gmail.com>
2023-08-15 17:33:47 +0900

    Fix bug in app.py

--- app.py
+++ app.py
@@ -8,26 +8,26 @@ pl.seed_everything(42)
 
 
 with gr.Blocks() as demo:
-    gr.Markdown("Flip text or image files using this demo.")
-    with gr.Tab("Flip Text"):
-        with gr.Row():
-            input_img = gr.Image()
-            replace_img = gr.Image()
-            target_img = gr.Image()
-        repaint_button = gr.Button("repaint")
+    gr.Markdown("Out-painting & Re-painting demo")
     with gr.Tab("Out painting"):
         with gr.Row():
-            input_img = gr.Image()
+            first_img = gr.Image()
             outpaint_img = gr.Image()
         left_border = gr.Textbox(label="left border")
         right_border = gr.Textbox(label="right border")
         top_border = gr.Textbox(label="top border")
         bottom_border = gr.Textbox(label="bottom border")
         outpaint_button = gr.Button("Paint")
+    with gr.Tab("Repainting"):
+        with gr.Row():
+            second_img = gr.Image()
+            replace_img = gr.Image()
+            target_img = gr.Image()
+        repaint_button = gr.Button("Paint")
 
-    repaint_button.click(sepia, inputs=input_img,
-                         outputs=[replace_img, target_img])
-    outpaint_button.click(image_mod, inputs=[input_img, left_border, top_border, right_border, bottom_border],
+    outpaint_button.click(image_mod, inputs=[first_img, left_border, top_border, right_border, bottom_border],
                           outputs=outpaint_img)
+    repaint_button.click(sepia, inputs=second_img,
+                         outputs=[replace_img, target_img])
 
 demo.launch()
