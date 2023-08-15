bef392729addee6b5a56c8430b9411bd97351ff2
SIC98 <shininchul98@gmail.com>
2023-08-15 19:38:17 +0900

    Function name updated

--- prepare_image.py
+++ prepare_image.py
@@ -1,22 +1,22 @@
 from PIL import Image
-from run_gradio import image_mod
 import os
 
-# directory/folder path
-directory_path = 'coco2014/val2014/'
+from out_painting import outpaint
+
+coco_data_path = 'coco2014/val2014/'
 out_painted_path = 'coco2014/out_painted/'
 
 # list to store files
-all_files_and_dirs = os.listdir(directory_path)
+all_files_and_dirs = os.listdir(coco_data_path)
 
 
 for item in all_files_and_dirs[:100]:
-    full_path = os.path.join(directory_path, item)
+    full_path = os.path.join(coco_data_path, item)
     if os.path.isfile(full_path):
 
         image = Image.open(full_path)
         print(image.size)
-        image = image_mod(image, 256, 256, 256, 256)
+        image = outpaint(image, 256, 256, 256, 256)
         image.save(
             os.path.join(out_painted_path, item)
         )
